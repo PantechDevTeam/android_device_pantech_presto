fe40ba286e97a0905ebeede6739e6691bd0d3774
jied83 <jied83@gmail.com>
2013-03-23 17:08:07 +0400

    make patchs, make apply_patchs.sh to apply patchs, fix storage list now work move to sd function
    Change-Id: I843f50a123142bc62f6f8294e400af536cf288ce

---
+++ apply_patchs.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+CM_DIR=../../../
+FRAMEWORK_AV=../../../frameworks/av
+FRAMEWORK_BASE=../../../frameworks/base
+HARDWARE=../../../hardware
+PATCH_DIR=device/pantech/presto/patchs
+
+## patch CameraParameters.cpp
+cd $CM_DIR
+patch -p0 -i $PATCH_DIR/camera_1.patch
+## patch CameraParameters.h
+patch -p0 -i $PATCH_DIR/camera_2.patch
+## Patch PowerManagerService.java
+patch -p0 -i $PATCH_DIR/proximity.patch
+## Patch libhardware_legacy
+patch -p0 -i $PATCH_DIR/wifi.patch
