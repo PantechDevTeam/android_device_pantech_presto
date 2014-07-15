a3ecd9a23a2a6560755a0966c868928579f25193
marduk191 <marduk191@gmail.com>
2014-07-14 19:51:34 -0500

    Update presto_patchs.sh for new patches

--- presto_patchs.sh
+++ presto_patchs.sh
@@ -15,6 +15,10 @@ do
             patch -p1 < $PATCH_DIR/proximity_sensor.patch
             ## Patch libhardware_legacy
             patch -p1 < $PATCH_DIR/custom_wific.patch
+	        ## Add bcm4329 driver
+            patch -p1 < $PATCH_DIR/bcm4329.patch
+	        ## Patch wpa_supplicant_8
+            patch -p1 < $PATCH_DIR/wpa_supplicant_8.patch
             ;;
         r)
             ## patch CameraParameters
@@ -23,6 +27,10 @@ do
             patch -Rp1 < $PATCH_DIR/proximity_sensor.patch
             ## Patch libhardware_legacy
             patch -Rp1 < $PATCH_DIR/custom_wific.patch
+            ## Remove bcm4329 driver
+            patch -Rp1 < $PATCH_DIR/bcm4329.patch
+	        ## Patch wpa_supplicant_8
+            patch -Rp1 < $PATCH_DIR/wpa_supplicant_8.patch
             ;;
         *)
             echo "invalid option -$OPTARG" 
